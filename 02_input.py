0b801f1b830102553376ac3df08e0ef1983f8550
Marcelo Genu Beserra <64420562+arreseb@users.noreply.github.com>
2023-10-09 22:28:56 -0300

    Create 02_input.py
    aprendendo a usar input

---
+++ 02_input.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+#https://www.youtube.com/watch?v=FNqdV5Zb_5Q&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=9
+
+nome = input('Qual o seu nome? ',)
+print('Olá, ', nome)
+print("É um prazer te conhecer, {}".format(nome))
