d894ec8ed56186a35756421f72d2d3d34d6df3ac
Marcelo Genu Beserra <64420562+arreseb@users.noreply.github.com>
2023-10-10 02:26:33 +0000

    musica-commit

--- Mundo_1 - Basicos/21_MP3.py
+++ Mundo_1 - Basicos/21_MP3.py
@@ -1,1 +1,7 @@
 #Faça um programa que abra e reproduza o áudio de um arquivo mp3
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('business.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
